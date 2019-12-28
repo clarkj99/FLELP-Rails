772ecdcaf41b63060f92c58d2277e4147e018ba3
clarkj99 <clark@stontech.net>
2019-12-28 08:20:54 -0500

    businesses controller show method

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,24 +1,23 @@
 class Api::V1::BusinessesController < ApplicationController
-  # skip_before_action :authorized, only: [:index, :show]
-  before_action :find_location
+  skip_before_action :authorized, only: [:show]
+  before_action :find_location, only: [:index]
 
   def index
     key = ENV["YELP_API_KEY"]
     loc = [@location.address1, @location.city, @location.state, @location.zip].join(" ")
     term = "restaurants"
-    url = "https://api.yelp.com/v3/businesses/search?limit=50&rterm=" + term + "&location=" + loc
+    url = "https://api.yelp.com/v3/businesses/search?sortby=distance&limit=50&rterm=" + term + "&location=" + loc
     headers = { authorization: "Bearer " + key }
     response = RestClient.get(url, headers)
     render json: response, status: :ok
   end
 
   def show
-  end
-
-  def create
-  end
-
-  def destroy
+    key = ENV["YELP_API_KEY"]
+    url = "https://api.yelp.com/v3/businesses/" + params[:id]
+    headers = { authorization: "Bearer " + key }
+    response = RestClient.get(url, headers)
+    render json: response, status: :ok
   end
 
   private
